b7379f9e5e5e2b75bc5a78d8210d8d156fbd23cd
Philippe Antoine <pantoine@oisf.net>
2023-05-30 10:12:31 +0200

    quadrafuzz

--- src/tests/fuzz/fuzz_predefpcap_aware.c
+++ src/tests/fuzz/fuzz_predefpcap_aware.c
@@ -33,6 +33,9 @@
 
 int LLVMFuzzerInitialize(const int *argc, char ***argv);
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
+size_t LLVMFuzzerCustomMutator(uint8_t *Data, size_t size, size_t MaxSize, unsigned int Seed);
+size_t LLVMFuzzerMutate(uint8_t *Data, size_t size, size_t MaxSize);
+char *debug = NULL;
 
 static int initialized = 0;
 ThreadVars tv;
@@ -49,7 +52,56 @@ char *filepath = NULL;
 int LLVMFuzzerInitialize(const int *argc, char ***argv)
 {
     filepath = dirname(strdup((*argv)[0]));
+    debug = getenv("FUZZ_DEBUG");
     return 0;
+}
+
+size_t LLVMFuzzerCustomMutator(uint8_t *Data, size_t size, size_t MaxSize, unsigned int Seed)
+{
+    if (Seed & 1 == 0) {
+        return LLVMFuzzerMutate(Data, size, MaxSize); // qsize
+    }
+    uint32_t crc = Seed >> 1;
+    uint8_t pattern_repeat_len = 1 + (crc & 0xFF);
+    uint32_t pattern_repeat_nb = 512 * (((crc >> 8) & 0xFF) + 1);
+    uint16_t prefix_postfix_ratio = (crc >> 16);
+
+    if (pattern_repeat_len > size) {
+        pattern_repeat_len = (uint8_t)size;
+    }
+    size_t prefix_postfix_len = size - pattern_repeat_len;
+    size_t prefix_len = (prefix_postfix_len * prefix_postfix_ratio) / 0xFFFF;
+    size_t postfix_len = prefix_postfix_len - prefix_len;
+    size_t qsize = prefix_len + (pattern_repeat_nb * pattern_repeat_len) + postfix_len;
+    if (qsize > MaxSize) {
+        pattern_repeat_nb -= 1 + (qsize - MaxSize) / pattern_repeat_len;
+        qsize = prefix_len + (pattern_repeat_nb * pattern_repeat_len) + postfix_len;
+    }
+    if (debug) {
+        printf("size = %zu\n", size);
+        printf("MaxSize = %zu\n", MaxSize);
+        printf("Seed = %u\n", Seed);
+        printf("pattern_repeat_len = %hhu\n", pattern_repeat_len);
+        printf("pattern_repeat_nb = %u\n", pattern_repeat_nb);
+        printf("prefix_postfix_ratio = %hu\n", prefix_postfix_ratio);
+        printf("prefix_postfix_len = %zu\n", prefix_postfix_len);
+        printf("prefix_len = %zu\n", prefix_len);
+        printf("postfix_len = %zu\n", postfix_len);
+        printf("qsize = %zu\n", qsize);
+    }
+    if (MaxSize < qsize) {
+        printf("small %zx vs %zx\n", MaxSize, qsize);
+        return size;
+    }
+    uint8_t *qdata = malloc(qsize);
+    memcpy(qdata, Data, prefix_len);
+    for (size_t i = 0; i < pattern_repeat_nb; i++) {
+        memcpy(qdata + prefix_len + i * pattern_repeat_len, Data + prefix_len, pattern_repeat_len);
+    }
+    memcpy(qdata + qsize - postfix_len, Data + size - postfix_len, postfix_len);
+    memcpy(Data, qdata, qsize);
+    free(qdata);
+    return qsize;
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
