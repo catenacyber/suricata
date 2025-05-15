2f00a0e8534d8a54d913862674400af969548a40
Philippe Antoine <pantoine@oisf.net>
2025-05-15 14:15:07 +0200

    flow: adds a tunnel identifier
    Ticket: 7674
    
    To distinguish flows with the same 5-tuple but coming from different
    tunnel sources.

--- src/output-json.c
+++ src/output-json.c
@@ -908,7 +908,9 @@ SCJsonBuilder *CreateEveHeader(const Packet *p, enum SCOutputJsonLogDirection di
     } else if (PacketIsIPv6(p)) {
         SCJbSetUint(js, "ip_v", 6);
     }
-
+    if (f->tunnel_id > 0 && f->tunnel_id != PKT_TUNNEL_UNKNOWN) {
+        SCJbSetUint(js, "tunnel_id", f->tunnel_id);
+    }
     /* icmp */
     switch (p->proto) {
         case IPPROTO_ICMP:
